e567c6489ec78c0f09f448fcd5bd63eddb3325f1
HelloWorldCoder <HelloWorldCoder-China@outlook.com>
2025-10-18 13:13:01 +0800

    Init

---
+++ src/main/java/DragonMCSoftwares/DragonCore.java
@@ -0,0 +1,17 @@
+package DragonMCSoftwares;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DragonCore extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
